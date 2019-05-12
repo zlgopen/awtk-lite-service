cc001db99ed808169b0254c618e2c9581949448e
xianjimli <xianjimli@gmail.com>
2019-05-12 08:45:34 +0800

    update docs

--- src/http/http.h
+++ src/http/http.h
@@ -39,13 +39,12 @@ BEGIN_C_DECLS
  * @method http_request
  * 启动一个lite service线程处理HTTP请求，在处理过程会把事件通过request->on_event通知调用者。
  *
- * * request->on_event在UI线程中执行，所以在该函数中可以操作GUI的控件。
+ *注意：
  *
- * * request只能在request->response的fail标志或done标志为TRUE后才能销毁。
- *
- * * 可以通过http_request_abort取消请求，取消请求异步执行，可能会有一定延迟。
- *
- * * 本函数适合一般的REST API，不适合传输大文件，也不适合同时发送大量请求。
+ *> * request->on_event在UI线程中执行，所以在该函数中可以操作GUI的控件。
+ *> * request只能在request->response的fail标志或done标志为TRUE后才能销毁。
+ *> * 可以通过http_request_abort取消请求，取消请求异步执行，可能会有一定延迟。
+ *> * 本函数适合一般的REST API，不适合传输大文件，也不适合同时发送大量请求。
  *
  * @param {http_request_t*} request HTTP请求对象。
  *
